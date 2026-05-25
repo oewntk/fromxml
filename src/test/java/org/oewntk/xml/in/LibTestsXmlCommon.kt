2f74dd49588442ff841429d98369456e44a78c23
1313ou <1313ou@gmail.com>
2026-05-25 21:44:33 +0200

    warning message

--- src/test/java/org/oewntk/xml/in/LibTestsXmlCommon.kt
+++ src/test/java/org/oewntk/xml/in/LibTestsXmlCommon.kt
@@ -16,13 +16,13 @@ object LibTestsXmlCommon {
 
     val model: CoreModel by lazy {
         if (source == null) {
-            Tracing.psErr.println("Define serialized source file dir with -DSOURCE=path")
+            Tracing.psErr.println("Define XML source file dir with -DSOURCE=path")
             throw AssertionError("SOURCE not defined")
         }
         val inDir = File(source)
         Tracing.psInfo.printf("source=%s%n", inDir.absolutePath)
         if (!inDir.exists()) {
-            Tracing.psErr.println("Define YAML source dir that exists")
+            Tracing.psErr.println("Define XML source dir that exists")
             Assert.fail()
         }
         CoreFactory(inDir).get()!!
