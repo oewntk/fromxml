952dc988de22334a309f11750f5aa648c498f666
1313ou <1313ou@gmail.com>
2026-06-06 08:45:22 +0200

    senses now have a reference to lex, not the embedded lex

--- src/main/java/org/oewntk/xml/in/Parser.kt
+++ src/main/java/org/oewntk/xml/in/Parser.kt
@@ -281,7 +281,7 @@ open class Parser(
         // adj position
         val adjPosition = adjPositionAttr.ifEmpty { null }
 
-        return Sense(sensekey, lex, SynsetType.fromChar(type), n, synsetId, examples, verbFrames, adjPosition, relations)
+        return Sense(sensekey, lex.key, SynsetType.fromChar(type), n, synsetId, examples, verbFrames, adjPosition, relations)
     }
 
     companion object {
