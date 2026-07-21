8c0291da9ab74fde3fab9f08b1d447da8686ee9c
1313ou <1313ou@gmail.com>
2026-07-21 07:39:37 +0200

    unnecessary if

--- src/test/java/org/oewntk/xml/in/LibTestsXmlCommon.kt
+++ src/test/java/org/oewntk/xml/in/LibTestsXmlCommon.kt
@@ -12,8 +12,7 @@ object LibTestsXmlCommon {
 
     private val source: String? = System.getProperty("SOURCE")
 
-    val silent = if (System.getProperties().containsKey("VERBOSE")) false
-    else if (System.getProperties().containsKey("SILENT")) true
+    val silent = !System.getProperties().containsKey("VERBOSE") && if (System.getProperties().containsKey("SILENT")) true
     else true
 
     val ps: PrintStream = if (!silent) Tracing.psInfo else Tracing.psNull
